197ba5971f69b2d9c0711750ac6a3ddaba42a7a9
Chimbori <hello@chimbori.com>
2016-09-08 10:05:23 -0700

    Cleanup OutputFormatter: meaningful variable names, braces around each conditional, renamed a few methods for clarity. No functional changes.

--- src/main/java/com/chimbori/snacktroid/OutputFormatter.java
+++ src/main/java/com/chimbori/snacktroid/OutputFormatter.java
@@ -25,9 +25,9 @@ class OutputFormatter implements Extractor.Formatter {
 
   public String getFormattedText(Element topNode) {
     removeNodesWithNegativeScores(topNode);
-    StringBuilder sb = new StringBuilder();
-    append(topNode, sb, NODES_TO_KEEP_SELECTOR);
-    String str = StringUtils.innerTrim(sb.toString());
+    StringBuilder buffer = new StringBuilder();
+    appendContentFromNode(topNode, buffer, NODES_TO_KEEP_SELECTOR);
+    String str = StringUtils.innerTrim(buffer.toString());
     if (str.length() > 100) {
       return str;
     }
@@ -45,8 +45,6 @@ class OutputFormatter implements Extractor.Formatter {
   public List<String> getTextList(Element topNode) {
     List<String> texts = new ArrayList<>();
     for (Element element : topNode.select(this.NODES_TO_KEEP_SELECTOR)) {
-      // System.err.println(element);
-
       if (element.hasText()) {
         texts.add(element.text());
       }
@@ -58,67 +56,72 @@ class OutputFormatter implements Extractor.Formatter {
     Elements gravityItems = topNode.select("*[gravityScore]");
     for (Element item : gravityItems) {
       int score = Integer.parseInt(item.attr("gravityScore"));
-      if (score < 0 || item.text().length() < MIN_LENGTH_FOR_PARAGRAPHS)
+      if (score < 0 || item.text().length() < MIN_LENGTH_FOR_PARAGRAPHS) {
         item.remove();
+      }
     }
   }
 
-  private void append(Element node, StringBuilder sb, String tagName) {
+  private void appendContentFromNode(Element node, StringBuilder buffer, String tagName) {
     // is select more costly then getElementsByTag?
     MAIN:
     for (Element e : node.select(tagName)) {
       Element tmpEl = e;
       // check all elements until 'node'
       while (tmpEl != null && !tmpEl.equals(node)) {
-        if (unlikely(tmpEl))
+        if (isUnlikely(tmpEl)) {
           continue MAIN;
+        }
         tmpEl = tmpEl.parent();
       }
 
-      String text = node2Text(e);
-      if (text.isEmpty() || text.length() < MIN_LENGTH_FOR_PARAGRAPHS || text.length() > StringUtils.countLetters(text) * 2)
+      String text = getTextFromElement(e);
+      if (text.isEmpty() || text.length() < MIN_LENGTH_FOR_PARAGRAPHS || text.length() > StringUtils.countLetters(text) * 2) {
         continue;
+      }
 
-      sb.append(text);
-      sb.append("\n\n");
+      buffer.append(text);
+      buffer.append("\n\n");
     }
   }
 
-  private boolean unlikely(Node e) {
-    if (e.attr("class") != null && e.attr("class").toLowerCase().contains("caption"))
+  private boolean isUnlikely(Node e) {
+    String styleAttribute = e.attr("style");
+    String classAttribute = e.attr("class");
+    if (classAttribute != null && classAttribute.toLowerCase().contains("caption")) {
       return true;
-
-    String style = e.attr("style");
-    String clazz = e.attr("class");
-    return UNLIKELY_CSS_CLASSES.matcher(style).find() || UNLIKELY_CSS_CLASSES.matcher(clazz).find();
+    }
+    return UNLIKELY_CSS_CLASSES.matcher(styleAttribute).find() || UNLIKELY_CSS_CLASSES.matcher(classAttribute).find();
   }
 
-  void appendTextSkipHidden(Element e, StringBuilder accum) {
+  void appendTextSkipHidden(Element e, StringBuilder buffer) {
     for (Node child : e.childNodes()) {
-      if (unlikely(child))
+      if (isUnlikely(child)) {
         continue;
+      }
       if (child instanceof TextNode) {
         TextNode textNode = (TextNode) child;
         String txt = textNode.text();
-        accum.append(txt);
+        buffer.append(txt);
       } else if (child instanceof Element) {
         Element element = (Element) child;
-        if (accum.length() > 0 && element.isBlock() && !lastCharIsWhitespace(accum))
-          accum.append(" ");
-        else if (element.tagName().equals("br"))
-          accum.append(" ");
-        appendTextSkipHidden(element, accum);
+        if (buffer.length() > 0 && element.isBlock() && !lastCharIsWhitespace(buffer)) {
+          buffer.append(" ");
+        } else if (element.tagName().equals("br")) {
+          buffer.append(" ");
+        }
+        appendTextSkipHidden(element, buffer);
       }
     }
   }
 
-  private boolean lastCharIsWhitespace(StringBuilder accum) {
-    return accum.length() != 0 && Character.isWhitespace(accum.charAt(accum.length() - 1));
+  private boolean lastCharIsWhitespace(StringBuilder buffer) {
+    return buffer.length() != 0 && Character.isWhitespace(buffer.charAt(buffer.length() - 1));
   }
 
-  private String node2Text(Element el) {
-    StringBuilder sb = new StringBuilder(200);
-    appendTextSkipHidden(el, sb);
-    return sb.toString();
+  private String getTextFromElement(Element element) {
+    StringBuilder buffer = new StringBuilder();
+    appendTextSkipHidden(element, buffer);
+    return buffer.toString();
   }
 }
