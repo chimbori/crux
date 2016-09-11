47d2cc9504c01fdd5cf270f2ab8812b4d3c5c149
Chimbori <hello@chimbori.com>
2016-09-11 09:27:00 -0700

    Rename ExampleFilesTest to GoldenFilesTest.

--- src/test/java/com/chimbori/crux/GoldenFilesTest.java
+++ src/test/java/com/chimbori/crux/GoldenFilesTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-public class ExampleFilesTest {
+public class GoldenFilesTest {
   private Extractor extractor;
 
   @Before
