21fcf3cdba512833852676babc1407b74d9e873b
Chimbori <hello@chimbori.com>
2021-01-04 14:50:03 -0800

    Remove unused methods from StringUtils.

--- src/main/java/com/chimbori/crux/common/StringUtils.kt
+++ src/main/java/com/chimbori/crux/common/StringUtils.kt
@@ -22,62 +22,6 @@ fun String.removeWhiteSpace() = replace(WHITESPACE, " ").trim { it <= ' ' }
 private val WHITESPACE = "\\s+".toRegex()
 
 object StringUtils {
-  fun urlEncodeSpaceCharacter(url: String?) =
-      if (url.isNullOrEmpty()) {
-        null
-      } else {
-        url.trim { it <= ' ' }.replace(WHITESPACE, "%20")
-      }
-
-  /** Starts reading the encoding from the first valid character until an invalid encoding character occurs. */
-  fun encodingCleanup(str: String): String {
-    val sb = StringBuilder()
-    var startedWithCorrectString = false
-    for (i in 0 until str.length) {
-      val c = str[i]
-      if (Character.isDigit(c) || Character.isLetter(c) || c == '-' || c == '_') {
-        startedWithCorrectString = true
-        sb.append(c)
-        continue
-      }
-      if (startedWithCorrectString) break
-    }
-    return sb.toString().trim { it <= ' ' }
-  }
-
-  /** @return the longest substring as str1.substring(result[0], result[1]); */
-  fun getLongestSubstring(str1: String, str2: String?): String {
-    val res = longestSubstring(str1, str2)
-    return if (res == null || res[0] >= res[1]) "" else str1.substring(res[0], res[1])
-  }
-
-  private fun longestSubstring(str1: String?, str2: String?): IntArray? {
-    if (str1 == null || str1.isEmpty() || str2 == null || str2.isEmpty()) return null
-
-    // dynamic programming => save already identical length into array
-    // to understand this algo simply print identical length in every entry of the array
-    // i+1, j+1 then reuses information from i,j
-    // java initializes them already with 0
-    val num = Array(str1.length) { IntArray(str2.length) }
-    var maxlen = 0
-    var lastSubstrBegin = 0
-    var endIndex = 0
-    for (i in 0 until str1.length) {
-      for (j in 0 until str2.length) {
-        if (str1[i] == str2[j]) {
-          if (i == 0 || j == 0) num[i][j] = 1 else num[i][j] = 1 + num[i - 1][j - 1]
-          if (num[i][j] > maxlen) {
-            maxlen = num[i][j]
-            // generate substring from str1 => i
-            lastSubstrBegin = i - num[i][j] + 1
-            endIndex = i + 1
-          }
-        }
-      }
-    }
-    return intArrayOf(lastSubstrBegin, endIndex)
-  }
-
   fun countLetters(str: String) = str.count { Character.isLetter(it) }
 
   fun parseAttrAsInt(element: Element, attr: String?) = try {
@@ -85,7 +29,6 @@ object StringUtils {
   } catch (e: NumberFormatException) {
     0
   }
-
 
   fun cleanTitle(title: String) = if (title.lastIndexOf("|") > title.length / 2) {
     title.substring(0, title.indexOf("|")).trim()
@@ -100,27 +43,4 @@ object StringUtils {
   }
 
   private val BACKSLASH_HEX_SPACE_PATTERN = Pattern.compile("\\\\([a-zA-Z0-9]+) ") // Space is included.
-
-  /**
-   * Unescapes backslash-hex escaped strings in URLs (typically found in URLs used in CSS).
-   * The escaped pattern begins with a backslash and ends with a space. All characters between these
-   * two delimiters must be valid hex digits.
-   * E.g.
-   * "\3a " becomes ":"
-   * "\3d " becomes "="
-   * "\26 " becomes "&amp;"
-   */
-  fun unescapeBackslashHex(input: String?): String? {
-    var output = input
-        ?: return null
-    val matcher = BACKSLASH_HEX_SPACE_PATTERN.matcher(input)
-    while (matcher.find()) {
-      val backSlashHexSpace = matcher.group(0)
-      val hexUnicode = matcher.group(1)
-      val decimalUnicode = hexUnicode.trim { it <= ' ' }.toInt(16)
-      val unicode = String(Character.toChars(decimalUnicode))
-      output = output.replace(backSlashHexSpace, unicode)
-    }
-    return output
-  }
 }
--- src/test/java/com/chimbori/crux/common/StringUtilsTest.kt
+++ src/test/java/com/chimbori/crux/common/StringUtilsTest.kt
@@ -1,9 +1,6 @@
 package com.chimbori.crux.common
 
 import com.chimbori.crux.common.StringUtils.cleanTitle
-import com.chimbori.crux.common.StringUtils.encodingCleanup
-import com.chimbori.crux.common.StringUtils.getLongestSubstring
-import com.chimbori.crux.common.StringUtils.unescapeBackslashHex
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.net.MalformedURLException
@@ -30,14 +27,6 @@ class StringUtilsTest {
   }
 
   @Test
-  fun testLongestSubstring() {
-    assertEquals("hello how", getLongestSubstring("hi hello how are you?", "hello how"))
-    assertEquals(" people if ", getLongestSubstring("x now if people if todo?", "I know people if you"))
-    assertEquals("", getLongestSubstring("?", "people"))
-    assertEquals("people", getLongestSubstring(" people ", "people"))
-  }
-
-  @Test
   fun testImageProtocolRelative() {
     val result = try {
       URL(URL("http://de.wikipedia.org/wiki/Griechenland"), "//upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Greece.svg/150px-Flag_of_Greece.svg.png").toString()
@@ -45,25 +34,6 @@ class StringUtilsTest {
       "//upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Greece.svg/150px-Flag_of_Greece.svg.png"
     }
     assertEquals("http://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Greece.svg/150px-Flag_of_Greece.svg.png", result)
-  }
-
-  @Test
-  fun testEncodingCleanup() {
-    assertEquals("utf-8", encodingCleanup("utf-8"))
-    assertEquals("utf-8", encodingCleanup("utf-8\""))
-    assertEquals("utf-8", encodingCleanup("utf-8'"))
-    assertEquals("test-8", encodingCleanup(" test-8 &amp;"))
-  }
-
-  @Test
-  fun testUnescapeBackslashHexUrl() {
-    assertEquals(null, unescapeBackslashHex(null))
-    assertEquals("", unescapeBackslashHex(""))
-    assertEquals(":", unescapeBackslashHex("\\3a "))
-    assertEquals("::", unescapeBackslashHex("\\3a \\3a "))
-    assertEquals("=:", unescapeBackslashHex("\\3d \\3a "))
-    assertEquals("https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-1/cp0/e15/q65/p120x120/00000000_00000000000000000_0000000000000000000_n.jpg?efg=aaaaaaaaaaaa&oh=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&oe=abcdefgh",
-        unescapeBackslashHex("https\\3a //scontent-sjc2-1.xx.fbcdn.net/v/t1.0-1/cp0/e15/q65/p120x120/00000000_00000000000000000_0000000000000000000_n.jpg?efg\\3d aaaaaaaaaaaa\\26 oh\\3d aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\\26 oe\\3d abcdefgh"))
   }
 
   @Test
