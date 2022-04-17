3dd5a2a0293c9c5812a9cf54017dd52b12607ec0
Chimbori <hello@chimbori.com>
2022-04-17 15:34:07 -0700

    Allow passing in a null `HttpUrl` as the canonical URL for each function that extracts URLs. If one isn’t provided, Crux will try to parse every detected URL as an independent URL.

--- src/main/kotlin/com/chimbori/crux/extractors/MetadataHelpers.kt
+++ src/main/kotlin/com/chimbori/crux/extractors/MetadataHelpers.kt
@@ -4,6 +4,7 @@ import com.chimbori.crux.common.cleanTitle
 import com.chimbori.crux.common.nullIfBlank
 import com.chimbori.crux.common.removeWhiteSpace
 import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import org.jsoup.nodes.Document
 
 internal fun Document.extractTitle(): String? = (
@@ -42,29 +43,30 @@ internal fun Document.extractKeywords(): List<String> =
     .split("\\s*,\\s*".toRegex())
     .filter { it.isNotBlank() }
 
-internal fun Document.extractFaviconUrl(baseUrl: HttpUrl): HttpUrl? = (
+internal fun Document.extractFaviconUrl(baseUrl: HttpUrl?): HttpUrl? = (
     findLargestIcon(select("head link[rel~=icon]"))
       ?: findLargestIcon(select("head link[rel~=ICON]"))
       ?: findLargestIcon(select("head link[rel^=apple-touch-icon]"))
-    )?.let { baseUrl.resolve(it) }
+    )?.let { baseUrl?.resolve(it) ?: it.toHttpUrl() }
 
-internal fun Document.extractImageUrl(baseUrl: HttpUrl): HttpUrl? = (
+internal fun Document.extractImageUrl(baseUrl: HttpUrl?): HttpUrl? = (
     // Twitter Cards and Open Graph images are usually higher quality, so rank them first.
     select("head meta[name=twitter:image]").attr("content").nullIfBlank()
       ?: select("head meta[property=og:image]").attr("content").nullIfBlank()
       // image_src or thumbnails are usually low quality, so prioritize them *after* article images.
       ?: select("link[rel=image_src]").attr("href").nullIfBlank()
       ?: select("head meta[name=thumbnail]").attr("content").nullIfBlank()
-    )?.let { baseUrl.resolve(it) }
+    )?.let { baseUrl?.resolve(it) ?: it.toHttpUrl() }
 
-internal fun Document.extractFeedUrl(baseUrl: HttpUrl): HttpUrl? = (
+internal fun Document.extractFeedUrl(baseUrl: HttpUrl?): HttpUrl? = (
     select("link[rel=alternate]").select("link[type=application/rss+xml]").attr("href").nullIfBlank()
       ?: select("link[rel=alternate]").select("link[type=application/atom+xml]").attr("href").nullIfBlank()
-    )?.let { baseUrl.resolve(it) }
+    )?.let { baseUrl?.resolve(it) ?: it.toHttpUrl() }
 
-internal fun Document.extractAmpUrl(baseUrl: HttpUrl): HttpUrl? =
-  select("link[rel=amphtml]").attr("href").nullIfBlank()?.let { baseUrl.resolve(it) }
+internal fun Document.extractAmpUrl(baseUrl: HttpUrl?): HttpUrl? =
+  select("link[rel=amphtml]").attr("href").nullIfBlank()
+    ?.let { baseUrl?.resolve(it) ?: it.toHttpUrl() }
 
-internal fun Document.extractVideoUrl(baseUrl: HttpUrl): HttpUrl? =
+internal fun Document.extractVideoUrl(baseUrl: HttpUrl?): HttpUrl? =
   select("head meta[property=og:video]").attr("content").nullIfBlank()
-    ?.let { baseUrl.resolve(it) }
+    ?.let { baseUrl?.resolve(it) ?: it.toHttpUrl() }
