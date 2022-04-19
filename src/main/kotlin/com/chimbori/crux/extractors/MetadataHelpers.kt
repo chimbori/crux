56649427e16c6522a61317e8888f512cf69ac2ac
Chimbori <hello@chimbori.com>
2022-04-18 21:44:34 -0700

    Look for all metadata tags even outside `<head>`, in case a site’s markup is broken.

--- src/main/kotlin/com/chimbori/crux/extractors/MetadataHelpers.kt
+++ src/main/kotlin/com/chimbori/crux/extractors/MetadataHelpers.kt
@@ -9,34 +9,34 @@ import org.jsoup.nodes.Document
 
 internal fun Document.extractTitle(): String? = (
     title().nullIfBlank()
-      ?: select("head title").text().nullIfBlank()
-      ?: select("head meta[name=title]").attr("content").nullIfBlank()
-      ?: select("head meta[property=og:title]").attr("content").nullIfBlank()
-      ?: select("head meta[name=twitter:title]").attr("content").nullIfBlank()
+      ?: select("title").text().nullIfBlank()
+      ?: select("meta[name=title]").attr("content").nullIfBlank()
+      ?: select("meta[property=og:title]").attr("content").nullIfBlank()
+      ?: select("meta[name=twitter:title]").attr("content").nullIfBlank()
     )?.cleanTitle()?.nullIfBlank()
 
 internal fun Document.extractCanonicalUrl(): String? = (
-    select("head link[rel=canonical]").attr("href").nullIfBlank()
-      ?: select("head meta[property=og:url]").attr("content").nullIfBlank()
-      ?: select("head meta[name=twitter:url]").attr("content").nullIfBlank()
+    select("link[rel=canonical]").attr("href").nullIfBlank()
+      ?: select("meta[property=og:url]").attr("content").nullIfBlank()
+      ?: select("meta[name=twitter:url]").attr("content").nullIfBlank()
     )?.removeWhiteSpace()?.nullIfBlank()
 
 internal fun Document.extractDescription(): String? = (
-    select("head meta[name=description]").attr("content").nullIfBlank()
-      ?: select("head meta[property=og:description]").attr("content").nullIfBlank()
-      ?: select("head meta[name=twitter:description]").attr("content").nullIfBlank()
+    select("meta[name=description]").attr("content").nullIfBlank()
+      ?: select("meta[property=og:description]").attr("content").nullIfBlank()
+      ?: select("meta[name=twitter:description]").attr("content").nullIfBlank()
     )?.removeWhiteSpace()?.nullIfBlank()
 
 internal fun Document.extractSiteName(): String? = (
-    select("head meta[property=og:site_name]").attr("content").nullIfBlank()
-      ?: select("head meta[name=application-name]").attr("content").nullIfBlank()
+    select("meta[property=og:site_name]").attr("content").nullIfBlank()
+      ?: select("meta[name=application-name]").attr("content").nullIfBlank()
     )?.removeWhiteSpace()?.nullIfBlank()
 
 internal fun Document.extractThemeColor(): String? =
   select("meta[name=theme-color]").attr("content").nullIfBlank()
 
 internal fun Document.extractKeywords(): List<String> =
-  select("head meta[name=keywords]").attr("content")
+  select("meta[name=keywords]").attr("content")
     .removeWhiteSpace()
     .removePrefix("[")
     .removeSuffix("]")
@@ -53,11 +53,11 @@ internal fun Document.extractFaviconUrl(baseUrl: HttpUrl?): HttpUrl? = (
 
 internal fun Document.extractImageUrl(baseUrl: HttpUrl?): HttpUrl? = (
     // Twitter Cards and Open Graph images are usually higher quality, so rank them first.
-    select("head meta[name=twitter:image]").attr("content").nullIfBlank()
-      ?: select("head meta[property=og:image]").attr("content").nullIfBlank()
+    select("meta[name=twitter:image]").attr("content").nullIfBlank()
+      ?: select("meta[property=og:image]").attr("content").nullIfBlank()
       // image_src or thumbnails are usually low quality, so prioritize them *after* article images.
       ?: select("link[rel=image_src]").attr("href").nullIfBlank()
-      ?: select("head meta[name=thumbnail]").attr("content").nullIfBlank()
+      ?: select("meta[name=thumbnail]").attr("content").nullIfBlank()
     )?.let { baseUrl?.resolve(it) ?: it.toHttpUrlOrNull() }
 
 internal fun Document.extractFeedUrl(baseUrl: HttpUrl?): HttpUrl? = (
@@ -70,5 +70,5 @@ internal fun Document.extractAmpUrl(baseUrl: HttpUrl?): HttpUrl? =
     ?.let { baseUrl?.resolve(it) ?: it.toHttpUrlOrNull() }
 
 internal fun Document.extractVideoUrl(baseUrl: HttpUrl?): HttpUrl? =
-  select("head meta[property=og:video]").attr("content").nullIfBlank()
+  select("meta[property=og:video]").attr("content").nullIfBlank()
     ?.let { baseUrl?.resolve(it) ?: it.toHttpUrlOrNull() }
