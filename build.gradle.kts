7c19656c294219c0bb21c1d8faa84ef81f60043c
Chimbori <hello@chimbori.com>
2022-10-21 19:01:52 -0700

    Update `com.vanniktech.maven.publish` from `0.21.0` to `0.22.0`

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   kotlin("jvm") version "1.7.20"
   id("com.github.ben-manes.versions") version "0.43.0"
-  id("com.vanniktech.maven.publish") version "0.21.0"
+  id("com.vanniktech.maven.publish") version "0.22.0"
 }
 
 repositories {
@@ -18,7 +18,7 @@ buildscript {
   dependencies {
     classpath(kotlin("gradle-plugin", version = "1.7.20"))
     classpath("com.github.ben-manes:gradle-versions-plugin:0.43.0")
-    classpath("com.vanniktech:gradle-maven-publish-plugin:0.21.0")
+    classpath("com.vanniktech:gradle-maven-publish-plugin:0.22.0")
   }
 }
 
