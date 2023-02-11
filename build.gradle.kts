e8157cd2845695c47996d5ba13ddefd9df85e4aa
Chimbori <hello@chimbori.com>
2023-02-10 18:17:42 -0800

    Update `com.github.ben-manes.versions` from `0.44.0` to `0.45.0`

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   kotlin("jvm") version "1.8.10"
-  id("com.github.ben-manes.versions") version "0.44.0"
+  id("com.github.ben-manes.versions") version "0.45.0"
   id("com.vanniktech.maven.publish") version "0.22.0"
 }
 
@@ -17,7 +17,7 @@ buildscript {
   }
   dependencies {
     classpath(kotlin("gradle-plugin", version = "1.8.10"))
-    classpath("com.github.ben-manes:gradle-versions-plugin:0.44.0")
+    classpath("com.github.ben-manes:gradle-versions-plugin:0.45.0")
     classpath("com.vanniktech:gradle-maven-publish-plugin:0.22.0")
   }
 }
