a22f677f286bb12407bff5eb4bdea1a1e8c4f53e
Chimbori <hello@chimbori.com>
2023-07-15 16:41:25 -0700

    Introduce Version Catalogs to track all dependencies centrally in `libs.versions.toml`

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode.Strict
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "1.8.21"
-  id("com.github.ben-manes.versions") version "0.45.0"
-  id("com.vanniktech.maven.publish") version "0.24.0"
+  alias(libs.plugins.kotlin.jvm)
+  alias(libs.plugins.ben.manes.versions)
+  alias(libs.plugins.maven.publish)
 }
 
 repositories {
@@ -16,22 +16,20 @@ buildscript {
     mavenCentral()
   }
   dependencies {
-    classpath(kotlin("gradle-plugin", version = "1.8.21"))
-    classpath("com.github.ben-manes:gradle-versions-plugin:0.45.0")
-    classpath("com.vanniktech:gradle-maven-publish-plugin:0.25.2")
+    classpath(libs.kotlin.plugin)
   }
 }
 
 dependencies {
-  api("org.jsoup:jsoup:1.16.1")
-  api("com.squareup.okhttp3:okhttp:4.10.0")
+  api(libs.jsoup)
+  api(libs.okhttp)
 
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.0")
-  implementation("com.beust:klaxon:5.6")
+  implementation(libs.coroutines.core)
+  implementation(libs.klaxon)
 
-  testImplementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-  testImplementation("com.squareup.okhttp3:mockwebserver:4.10.0")
-  testImplementation("junit:junit:4.13.2")
+  testImplementation(libs.okhttp.mockwebserver)
+  testImplementation(libs.okhttp.logging)
+  testImplementation(libs.junit)
 }
 
 configurations.all {
